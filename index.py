07be8e1429c5f3e81b07215acf12d3b333d6f9ce
yacintheb <byacinthe@gmail.com>
2026-07-13 17:42:48 +0100

    Fix main menuhandling and serach function call

--- index.py
+++ index.py
@@ -2,7 +2,7 @@
 from afficher_contacts import afficher_contact
 from ajout_contact import ajouter_contact
 from supprimer_contact import supprimer_contact
-from rechercher_un_contact import rechercher_un_contact
+from rechercher_contact import rechercher_un_contact
 
 contacts = [] ## On crée la liste
 
@@ -11,8 +11,8 @@ print ("======== gestionnaire de contacts========")
 print ("Appuyez sur:\n1 pour afficher les contacts\n2 pour ajouter un contact\n3 pour supprimer un contact\n4 pour rechercher un contact\n5 pour quitter le programme")
 
 while True: # 2. Boucle infinie pour afficher le menu tant qu'on ne quitte pas
-    
-   try:
+
+    try:
         choix = int(input("Votre choix : "))
     except ValueError:
         print("Veuillez saisir un nombre entre 1 et 5.")
@@ -25,7 +25,7 @@ while True: # 2. Boucle infinie pour afficher le menu tant qu'on ne quitte pas
     elif choix == 3:
         supprimer_contact(contacts)
     elif choix == 4:
-        rechercher_contact(contacts)
+        rechercher_un_contact(contacts)
     elif choix == 5:
         print("Vous avez quitté le programme")
         break # Arrête la boucle while
