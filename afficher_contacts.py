52c44c917cc9fea722604027bbc4e6d72e07c80c
yacintheb <byacinthe@gmail.com>
2026-07-13 18:04:44 +0100

    fix handle empty contact list display

--- afficher_contacts.py
+++ afficher_contacts.py
@@ -1,7 +1,11 @@
 def afficher_contact (contacts):
     try:
-        with open("contacts.txt", "r") as f:
+        with open("contacts.txt", "r", encoding="utf-8") as f:
             contenu = f.read()
-            print (contenu)
+
+            if contenu.strip():
+                print(contenu)
+            else:
+                print("Vous n'avez pas de contacts enregistrés.")
     except FileNotFoundError:
-        print("vous n'avez pas de contacts enregistrés")
+        print("Vous n'avez pas de contacts enregistrés.")
